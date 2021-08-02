ed47e0f37dc9b78b1329e0ffd453983dda0cc367
wagner <wagnerclara@hotmail.com>
2021-08-02 14:28:43 -0300

    urna

---
+++ etapas.js
@@ -0,0 +1,54 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Sissi de mainha',
+                partido: 'SISSI',
+                fotos: [
+                    {url:'sissi.jpg', legenda: 'Vereador'}
+                ]
+            },
+            { 
+
+                numero: 72111,
+                nome: 'Sissi de emy',
+                partido: 'SISSI',
+                fotos: [
+                    {url:'sissi.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos: [
+            {
+                numero: 72,
+                nome: 'Sissi',
+                partido: 'SISSI',
+
+                fotos: [
+                    {url:'sissi.jpg', legenda: 'Prefeito'},
+                    {url:'boquinha.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+               
+            },
+
+            {
+                numero: 62,
+                nome: 'boquinha',
+                partido: 'SISSI',
+
+                fotos: [
+                    {url:'family.jpg', legenda: 'Prefeito'},
+                    {url:'sissi.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+                },
+
+        ]
+    }
+];
